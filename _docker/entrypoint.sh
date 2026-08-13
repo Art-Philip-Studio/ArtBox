4991828497d522a44ac7ee4ed37e98d4945e1f35
Art Philip Studio <studio.art.philip@gmail.com>
2026-08-13 14:34:39 -0500

    Persist user database inside /srv volume

--- _docker/entrypoint.sh
+++ _docker/entrypoint.sh
@@ -2,5 +2,7 @@
 set -e
 if [ -d /srv ]; then
   chown filebrowser:filebrowser /srv 2>/dev/null || true
+  mkdir -p /srv/.artbox 2>/dev/null || true
+  chown filebrowser:filebrowser /srv/.artbox 2>/dev/null || true
 fi
 exec su-exec filebrowser ./filebrowser
